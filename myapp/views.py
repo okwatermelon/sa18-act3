91c511156c5d166948d998dca2f4b2fa2fc4c647
Anthony Ybarra <ovfan@outlook.com>
2024-04-21 23:40:39 -0500

    final commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from .models import Product
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+def show(request, id):
+    user = get_object_or_404(Product, id=id)
+    return render(request, 'show.html', {'product': product})    
 # Create your views here.
